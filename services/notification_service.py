6b837b03d4c0726a7a9c117c73f6faa53970b5e4
Rohith Krishna <105430589+rk032@users.noreply.github.com>
2025-10-05 14:18:42 +0530

    Update notification_service.py

--- services/notification_service.py
+++ services/notification_service.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class NotificationService:
     def __init__(self):
         self.smtp_server = "smtp.gmail.com"  
         self.smtp_port = 587
-        self.email = "rohith142003@gmail.com"  
-        self.password = "ixnv rhou wtxs mkci"  
+        self.email = "temp@gmail.com"  
+        self.password = os.getenv("appassword") 
     
     def send_notification(self, address, message):
         user_email = "kokoxef203@bllibl.com"  
@@ -24,4 +27,5 @@ class NotificationService:
             server.quit()
             print("Notification sent!")
         except Exception as e:
+
             print(f"Notification failed: {e}")
